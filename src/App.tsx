2ef008c3a093078e8adb8f96c1d20ad386995d1a
Slezin Dmitry <slezindimawow@gmail.com>
2024-05-20 16:17:53 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ProgressBar } from 'react-bootstrap';
+
+function App() {
+ 
+  return (
+    <>
+     <Button variant="danger">Danger</Button>{' '}
+     return <ProgressBar now={60} />;
+    </>
+  )
+}
+
+export default App
